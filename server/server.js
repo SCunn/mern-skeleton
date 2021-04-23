6b7a3a5f31d209d454862a2ab9f3f81ce3a56622
scunn <shanecunn@hotmail.com>
2021-04-23 16:41:30 +0100

    Rendering templates at the root

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import devBundle from './devBundle' // for development mode, comment out in production
 import path from 'path'
+import template from './../template'
 
 const app = express()
 devBundle.compile(app)  // for development mode, comment out in production
 
 const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,'dist')))
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on %s.', port)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
